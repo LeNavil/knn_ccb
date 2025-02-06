52b3db552c613bf36b07401e43d2bb596835026f
Manunandhan Navilarasu Subrahmanyan <navilarasu-subrahmanyan.manunandhan@student-cs.fr>
2025-02-06 15:28:50 +0100

    one-hot encoding

---
+++ one_hot.py
@@ -0,0 +1,150 @@
+geo = ['Dense Forest,Grass Land', 'Sparse Forest,Dense Forest,Farms',
+       'Sparse Forest,Grass Land,Farms', 'Dense Forest,Farms',
+       'Sparse Forest,Farms', 'Sparse Forest,Dense Forest',
+       'River,Sparse Forest,Grass Land', 'River,Dense Forest,Farms', 'Coastal',
+       'Dense Forest', 'Sparse Forest,Farms,Lakes',
+       'River,Sparse Forest,Dense Forest,Farms', 'Sparse Forest,Grass Land',
+       'Grass Land', 'Sparse Forest', 'Farms',
+       'River,Sparse Forest,Dense Forest,Lakes',
+       'Sparse Forest,Dense Forest,Grass Land', 'Farms,Lakes',
+       'River,Barren Land,Sparse Forest',
+       'Barren Land,Sparse Forest,Dense Forest', 'Barren Land,Sparse Forest',
+       'River,Sparse Forest,Farms', 'Sparse Forest,Grass Land,Lakes',
+       'Barren Land,Sparse Forest,Dense Forest,Lakes', 'River,Farms',
+       'Sparse Forest,Lakes', 'River,Lakes',
+       'Barren Land,Sparse Forest,Grass Land',
+       'River,Barren Land,Sparse Forest,Dense Forest,Lakes',
+       'Barren Land,Sparse Forest,Lakes', 'Barren Land',
+       'Sparse Forest,Dense Forest,Lakes',
+       'Barren Land,Sparse Forest,Grass Land,Lakes',
+       'Sparse Forest,Dense Forest,Grass Land,Farms', 'N,A',
+       'Sparse Forest,Dense Forest,Grass Land,Lakes',
+       'River,Sparse Forest,Dense Forest,Grass Land',
+       'Dense Forest,Grass Land,Farms,Lakes', 'River,Sparse Forest',
+       'River,Sparse Forest,Dense Forest', 'River,Sparse Forest,Farms,Lakes',
+       'Grass Land,Farms,Lakes', 'River,Grass Land,Farms',
+       'River,Sparse Forest,Lakes', 'River,Sparse Forest,Grass Land,Farms',
+       'Coastal,Barren Land', 'Desert', 'River,Barren Land', 'River',
+       'Coastal,Sparse Forest,Grass Land',
+       'River,Sparse Forest,Grass Land,Farms,Lakes',
+       'Dense Forest,Grass Land,Farms', 'Sparse Forest,Dense Forest,Farms,Lakes',
+       'River,Sparse Forest,Grass Land,Lakes',
+       'River,Barren Land,Grass Land,Farms',
+       'Sparse Forest,Dense Forest,Grass Land,Farms,Lakes', 'Barren Land,Farms',
+       'River,Dense Forest,Grass Land', 'River,Dense Forest',
+       'Sparse Forest,Grass Land,Farms,Lakes',
+       'River,Barren Land,Sparse Forest,Grass Land', 'Barren Land,Grass Land',
+       'Coastal,Sparse Forest',
+       'River,Sparse Forest,Dense Forest,Grass Land,Farms',
+       'River,Sparse Forest,Dense Forest,Grass Land,Lakes',
+       'Dense Forest,Farms,Lakes', 'River,Sparse Forest,Dense Forest,Farms,Lakes',
+       'River,Dense Forest,Lakes', 'River,Barren Land,Dense Forest,Grass Land',
+       'Dense Forest,Grass Land,Lakes',
+       'Barren Land,Sparse Forest,Dense Forest,Grass Land',
+       'Barren Land,Sparse Forest,Farms', 'Grass Land,Lakes',
+       'Barren Land,Dense Forest,Farms', 'River,Barren Land,Sparse Forest,Farms',
+       'River,Barren Land,Sparse Forest,Lakes', 'Barren Land,Dense Forest,Lakes',
+       'Dense Forest,Lakes', 'Lakes',
+       'Barren Land,Sparse Forest,Dense Forest,Grass Land,Lakes',
+       'Coastal,Grass Land', 'Barren Land,Hills', 'Barren Land,Dense Forest,Hills',
+       'Hills,Desert', 'Barren Land,Sparse Forest,Hills', 'River,Grass Land',
+       'River,Grass Land,Lakes', 'Coastal,Sparse Forest,Lakes', 'Farms,Hills',
+       'Grass Land,Farms', 'Barren Land,Lakes',
+       'River,Sparse Forest,Dense Forest,Farms,Hills,Lakes',
+       'River,Barren Land,Grass Land',
+       'River,Barren Land,Sparse Forest,Farms,Lakes',
+       'Coastal,Dense Forest,Grass Land', 'Coastal,Dense Forest',
+       'Coastal,Sparse Forest,Dense Forest,Grass Land',
+       'River,Barren Land,Grass Land,Lakes',
+       'River,Barren Land,Sparse Forest,Dense Forest,Grass Land,Lakes',
+       'Barren Land,Sparse Forest,Desert', 'Sparse Forest,Desert',
+       'Barren Land,Sparse Forest,Dense Forest,Farms', 'River,Barren Land,Lakes',
+       'Barren Land,Farms,Lakes', 'River,Barren Land,Farms',
+       'Barren Land,Sparse Forest,Farms,Lakes',
+       'Barren Land,Dense Forest,Grass Land,Lakes', 'Barren Land,Dense Forest',
+       'Sparse Forest,Hills', 'Barren Land,Dense Forest,Grass Land',
+       'River,Barren Land,Sparse Forest,Dense Forest,Grass Land',
+       'River,Barren Land,Sparse Forest,Dense Forest',
+       'Barren Land,Grass Land,Lakes', 'River,Farms,Lakes', 'Coastal,Lakes',
+       'Coastal,Barren Land,Lakes', 'Grass Land,Desert', 'Lakes,Desert',
+       'River,Desert', 'Barren Land,Desert', 'Coastal,Barren Land,Sparse Forest',
+       'Coastal,River,Sparse Forest,Dense Forest',
+       'Coastal,Sparse Forest,Grass Land,Farms', 'Dense Forest,Hills',
+       'River,Dense Forest,Grass Land,Lakes',
+       'River,Barren Land,Sparse Forest,Grass Land,Lakes',
+       'Coastal,Sparse Forest,Dense Forest,Grass Land,Lakes',
+       'Barren Land,Sparse Forest,Grass Land,Farms',
+       'Coastal,Barren Land,Sparse Forest,Grass Land',
+       'Coastal,Barren Land,Dense Forest', 'Barren Land,Dense Forest,Farms,Lakes',
+       'Grass Land,Hills', 'Barren Land,Grass Land,Hills',
+       'Sparse Forest,Grass Land,Hills',
+       'River,Sparse Forest,Dense Forest,Grass Land,Farms,Lakes',
+       'Dense Forest,Hills,Lakes', 'River,Sparse Forest,Grass Land,Desert',
+       'Coastal,Sparse Forest,Farms,Lakes', 'Sparse Forest,Farms,Hills',
+       'Dense Forest,Farms,Hills,Lakes', 'River,Barren Land,Dense Forest',
+       'Barren Land,Sparse Forest,Dense Forest,Grass Land,Farms',
+       'Grass Land,Lakes,Desert', 'Snow', 'Coastal,Sparse Forest,Dense Forest',
+       'Coastal,Sparse Forest,Grass Land,Hills,Desert',
+       'Sparse Forest,Grass Land,Hills,Lakes', 'Sparse Forest,Grass Land,Desert',
+       'Coastal,Barren Land,Desert', 'River,Barren Land,Dense Forest,Lakes',
+       'River,Sparse Forest,Grass Land,Hills', 'Hills',
+       'River,Grass Land,Farms,Lakes', 'Dense Forest,Farms,Hills',
+       'River,Dense Forest,Farms,Lakes',
+       'Coastal,Sparse Forest,Grass Land,Lakes,Snow',
+       'Coastal,River,Sparse Forest',
+       'River,Barren Land,Sparse Forest,Grass Land,Farms',
+       'Barren Land,Farms,Hills', 'Coastal,Desert',
+       'Barren Land,Sparse Forest,Dense Forest,Farms,Lakes',
+       'Barren Land,Grass Land,Farms,Lakes',
+       'River,Barren Land,Sparse Forest,Dense Forest,Farms',
+       'River,Dense Forest,Grass Land,Farms,Lakes', 'Coastal,Farms',
+       'Barren Land,Sparse Forest,Grass Land,Farms,Lakes',
+       'Coastal,Sparse Forest,Farms', 'River,Dense Forest,Grass Land,Farms',
+       'Sparse Forest,Grass Land,Lakes,Snow', 'Barren Land,Grass Land,Farms',
+       'Sparse Forest,Lakes,Snow', 'River,Dense Forest,Grass Land,Hills',
+       'Barren Land,Hills,Lakes', 'Barren Land,Sparse Forest,Dense Forest,Hills',
+       'River,Barren Land,Grass Land,Farms,Lakes', 'Coastal,Dense Forest,Farms',
+       'Coastal,Sparse Forest,Grass Land,Lakes',
+       'River,Barren Land,Dense Forest,Grass Land,Farms',
+       'Coastal,Barren Land,Sparse Forest,Dense Forest,Grass Land']
+
+
+urban = ['Sparse Urban', 'Rural', 'Dense Urban', 'Urban Slum,Rural', 'N,A',
+         'Sparse Urban,Urban Slum', 'Sparse Urban,Industrial', 'Industrial',
+         'Dense Urban,Industrial', 'Urban Slum', 'Urban Slum,Industrial',
+         'Rural,Industrial', 'Sparse Urban,Dense Urban', 'Sparse Urban,Rural',
+         'Dense Urban,Urban Slum', 'Sparse Urban,Urban Slum,Industrial',
+         'Dense Urban,Rural']
+
+geo_set = set()
+urban_set = set()
+
+for x in geo:
+    unique_elems = x.split(',')
+    geo_set.update(set(unique_elems))
+    
+for y in urban:
+    unique_elems = y.split(',')
+    urban_set.update(set(unique_elems))
+    
+geo_set = geo_set.difference({'N','A'})
+urban_set = urban_set.difference({'N','A'})
+
+"""
+Which gives:
+
+geo_set = {'Sparse Forest', 'Desert', 'Coastal', 'River', 'Snow', 'Hills', 'Grass Land', 'Barren Land', 'Lakes', 'Dense Forest', 'Farms'}
+urban_set = {'Industrial', 'Rural', 'Sparse Urban', 'Urban Slum', 'Dense Urban'}
+
+"""
+
+for category in geo_set:
+    train_df[f'g_{category}'] = train_df['geography_type'].apply(lambda x: int(category in x.split(',')) if pd.notna(x) else 0)
+    train_df[f'g_NaN'] = train_df['geography_type'].apply(lambda x: 1 if x == 'N,A'else 0)
+
+for category in urban_set:
+    train_df[f'u_{category}'] = train_df['urban_type'].apply(lambda x: int(category in x.split(',')) if pd.notna(x) else 0)
+    train_df[f'u_NaN'] = train_df['urban_type'].apply(lambda x: 1 if x == 'N,A'else 0)
+
+# Drop original columns
+train_df.drop(columns=['geography_type', 'urban_type'], inplace=True)
